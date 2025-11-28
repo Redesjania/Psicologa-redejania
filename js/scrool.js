ac89e8b2b94d3c503a50b771ae5bf1199e9631a2
Redesjania <redesjania@yahoo.com.br>
2025-11-28 13:47:21 -0300

    redmi arquivos sites
    envio teste 01

---
+++ js/scrool.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#ctr', {
+        origin: 'right',
+        duration: 2000,
+        distance: '20%'
+    });
+    ScrollReveal().reveal('#ctb',{
+        origin: 'bottom',
+        duration: 1000,
+        distance: '10%'
+    })
+});
